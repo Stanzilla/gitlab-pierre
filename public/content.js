4ac30d5bfb212e04d2ab101d0138ca76466ee6e9
Benjamin Staneck <staneck@gmail.com>
2026-04-29 14:49:19 +0200

    build: simplify extension build process

--- public/content.js
+++ public/content.js
@@ -1,7 +1,7 @@
 const appUrl = chrome.runtime.getURL('pierre-app.js');
 
-console.info(`[GitLab Pierre] Loader ${__APP_VERSION__} active.`);
+console.info('[GitLab Pierre] Loader __APP_VERSION__ active.');
 
-void import(/* @vite-ignore */ appUrl).catch((error: unknown) => {
+void import(appUrl).catch((error) => {
   console.error('[GitLab Pierre] Failed to load Pierre app bundle.', error);
 });
